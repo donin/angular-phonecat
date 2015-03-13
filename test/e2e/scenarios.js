28f6f51144eea1e024fa61da427c01f9bfa5b6a9
Peter Donin <peter@donin.net>
2015-03-13 07:07:34 +0200

    finished with step-3

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,20 +10,27 @@ describe('PhoneCat App', function() {
       browser.get('app/index.html');
     });
 
+    var phoneList = element.all(by.repeater('phone in phones'));
+    var query = element(by.model('query'));
 
     it('should filter the phone list as a user types into the search box', function() {
 
-      var phoneList = element.all(by.repeater('phone in phones'));
-      var query = element(by.model('query'));
+       expect(phoneList.count()).toBe(3);
 
-      expect(phoneList.count()).toBe(3);
+       query.sendKeys('nexus');
+       expect(phoneList.count()).toBe(1);
+
+       query.clear();
+       query.sendKeys('motorola');
+       expect(phoneList.count()).toBe(2);
+    });
+
+    it('should display query in the title bar',function(){
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
 
       query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
     });
   });
 });
